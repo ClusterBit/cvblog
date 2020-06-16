d0b472dd4253665466e83f47041bf139437a069e
clusterbit <clusterbitgithub@gmail.com>
2020-06-16 14:45:01 +0300

    files

--- app/core/urls.py
+++ app/core/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='index'),
     path('<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
     path('signup/', views.signup.as_view(), name='signup'),
     path('signup/company/', views.signup_company, name='signup_company'),
